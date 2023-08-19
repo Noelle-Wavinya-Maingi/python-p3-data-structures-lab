cad3fb88372bbdb9a5116639250c527ce3f51e22
Noelle-Wavinya-Maingi <noelle.maingi@student.moringaschool.com>
2023-08-19 14:23:00 +0300

    Completed Assignment

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -16,23 +16,54 @@ spicy_foods = [
     },
 ]
 
+
 def get_names(spicy_foods):
-    pass
+    names = [food["name"] for food in spicy_foods]
+    return names
+
+get_names(spicy_foods)
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spiciest_food = [food for food in spicy_foods if food["heat_level"] > 5]
+    return spiciest_food
+
+
+spiciest_food = get_spiciest_foods(spicy_foods)
+print(spiciest_food)
+
 
 def print_spicy_foods(spicy_foods):
-    pass
+  for food in spicy_foods:
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {food['heat_level'] * '🌶'}")
+
+print_spicy_foods(spicy_foods)
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+    return None
+
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest_food = [food for food in spicy_foods if food["heat_level"] > 5]
+    print_spicy_foods(spiciest_food)
 
+print_spiciest_foods(spicy_foods)
+   
 def get_average_heat_level(spicy_foods):
-    pass
+    total_heat = sum(food["heat_level"] for food in spicy_foods)
+    num_foods = len(spicy_foods)
+    if num_foods > 0 :
+        average_heat = total_heat / num_foods
+        return int(average_heat)
+    else:
+        return 0
+
+# avg_heat = get_average_heat_level(spicy_foods)
+# print(f"Average Heat Level: {avg_heat}")
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
